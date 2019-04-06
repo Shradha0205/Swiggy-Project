434dcfc020841fb62e7e9d6ecebc22c976ed69dc
sn185127 <sn185127@WINSN185127-XBE.corp.ncr.com>
2019-04-07 01:02:38 +0530

    First Commit

---
+++ Swiggy_Project/src/main/java/Swiggy_Project/Swiggy_Project/App.java
@@ -0,0 +1,13 @@
+package Swiggy_Project.Swiggy_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
